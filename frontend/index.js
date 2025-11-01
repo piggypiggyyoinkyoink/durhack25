e33dfa9ca175505b6f414f1e358028433bb8e7e9
Jacob Lucas <Jacoblucashoyet123@gmail.com>
2025-11-01 16:49:03 +0000

    working on user input

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,6 @@
 import { getTalkSession } from 'https://cdn.jsdelivr.net/npm/@talkjs/core@1.5.8';
+const userId = ""
+
 let params = new URLSearchParams(document.location.search);
 let name = params.get("name");
 if (!name){
@@ -6,9 +8,25 @@ if (!name){
 
 }
 
+document.getElementById("EnterLogins").addEventListener('submit',async function(event){
+      event.preventDefault();
+      const Loginform = document.getElementById("EnterLogins")
+      console.log("hello1")
+      const LoginData = new FormData(Loginform);
+        let response =  await fetch('/api/user/${logininfomation.Username}')
+        let logininfomation = await response.json()
+        logininfomation.forEach(logininfomation => {
+            userId = logininfomation.Username
+
+                  })
+      
+           
+      
+})
+
 const appId = 'tKAe9pYx';
 
-const userId = name;
+
 console.log("USERID " + userId)
 const otherUserId = 'nina';
 const conversationId = 'my_conversation';
