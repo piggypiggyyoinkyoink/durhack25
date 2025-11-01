7d4db051db7c89733c91d917f4ed5a5f218ecaf1
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-11-01 15:29:15 +0000

    javascript unrelocated back to html, multipke users maybe?

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,15 @@
 import { getTalkSession } from 'https://cdn.jsdelivr.net/npm/@talkjs/core@1.5.8';
-          
+let params = new URLSearchParams(document.location.search);
+let name = params.get("name");
+if (!name){
+    let name = "Frank";
+
+}
+
 const appId = 'tKAe9pYx';
 
-const userId = 'frank';
+const userId = name;
+console.log("USERID " + userId)
 const otherUserId = 'nina';
 const conversationId = 'my_conversation';
 
@@ -13,7 +20,7 @@ const session = getTalkSession({
     userId,
 });
 
-session.currentUser.createIfNotExists({ name: 'Frank' });
+session.currentUser.createIfNotExists({ name: name });
 session.user(otherUserId).createIfNotExists({ name: 'Nina' });
 
 const conversation = session.conversation(conversationId);
