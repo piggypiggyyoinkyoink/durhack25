1de2b654f9ff30eed2ab519dd38135f9cfaf0f6c
Jacob Lucas <Jacoblucashoyet123@gmail.com>
2025-11-01 16:54:15 +0000

    Testing

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,5 @@
-import { getTalkSession } from 'https://cdn.jsdelivr.net/npm/@talkjs/core@1.5.8';
 const userId = ""
 
-let params = new URLSearchParams(document.location.search);
-let name = params.get("name");
-if (!name){
-    let name = "Frank";
-
-}
 
 document.getElementById("EnterLogins").addEventListener('submit',async function(event){
       event.preventDefault();
@@ -15,6 +8,7 @@ document.getElementById("EnterLogins").addEventListener('submit',async function(
       const LoginData = new FormData(Loginform);
         let response =  await fetch('/api/user/${logininfomation.Username}')
         let logininfomation = await response.json()
+        console.log(logininfomation)
         logininfomation.forEach(logininfomation => {
             userId = logininfomation.Username
 
@@ -23,24 +17,3 @@ document.getElementById("EnterLogins").addEventListener('submit',async function(
            
       
 })
-
-const appId = 'tKAe9pYx';
-
-
-console.log("USERID " + userId)
-const otherUserId = 'nina';
-const conversationId = 'my_conversation';
-
-const session = getTalkSession({
-    // @ts-ignore
-    host: 'durhack.talkjs.com',
-    appId,
-    userId,
-});
-
-session.currentUser.createIfNotExists({ name: name });
-session.user(otherUserId).createIfNotExists({ name: 'Nina' });
-
-const conversation = session.conversation(conversationId);
-conversation.createIfNotExists();
-conversation.participant(otherUserId).createIfNotExists();
