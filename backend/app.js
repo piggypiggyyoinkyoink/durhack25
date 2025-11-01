d2146784c0df4ef938d0a6ab0bb81ab2dc7014ee
OliClark <oliverclark2006@gmail.com>
2025-11-01 17:11:54 +0000

    Fixing the backend things

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const talkJS = require("@talkjs/core");
 const app = express ();
 const jwt = require("jsonwebtoken")
 const path = require("path");
@@ -6,23 +7,34 @@ const path = require("path");
 app.use(express.static("../frontend"));
 app.use(express.json());
 
-
+const session = getTalkSession({
+    // @ts-ignore
+    host: 'durhack.talkjs.com',
+    appId: 'tKAe9pYx',
+    userId,
+});
 
 module.exports=app;
 
+//User check
 app.get('/api/user/:username', function (req,resp){
 
     let user = req.params.username;
     console.log("USERID: " + user)
     // CALL TO CHECK EXISTS
-    check = true; // Placeholder for actual check
-    resp.json({exists: false});
+    session.currentUser.createIfNotExists({ name: user });
+    check = true;
+    resp.json({exists: check});
 
 });
 
 
-//User check
 //Converstion check
+app.get('api/conversation/:converstaionName'), function (req,resp){
+    let conversation = req.params.converstaionName;
+    
+}
+
 //Message passthrough
 
 //Rooms games
