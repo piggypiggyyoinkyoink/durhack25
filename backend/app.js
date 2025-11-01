2e3292479b995e7b5368c2c77996b24373b236c7
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-11-01 17:04:56 +0000

    messages for form1 now passed to server

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const jwt = require("jsonwebtoken")
 const app = express ();
 const path = require("path");
-
+const bodyParser = require("body-parser");
 app.use(express.static("../frontend"));
+app.use(express.json());
 
+app.use(bodyParser.json());
 
+app.post("/sendmessage", function(req,res){
+    
+    console.log(req.body);//contains message text, user ID and conversation ID.
+    res.sendStatus(200);
+})
 
 module.exports=app
