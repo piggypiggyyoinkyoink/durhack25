148cc1423075395daf49ca8d9cc1a60efd721c47
OliClark <oliverclark2006@gmail.com>
2025-11-02 11:41:37 +0000

    Fish

--- frontend/games.js
+++ frontend/games.js
@@ -6,3 +6,18 @@ if(!user || user == null){
 document.getElementById("pong").addEventListener("click", function(){
     window.location.href = "http://" + window.location.host+`/pong?name=${user}`;
 });
+
+document.getElementById("noGames").addEventListener("click", function(){
+    TriggerToast("Sorry, this game is not available yet. Please check back later!", "Game Unavailable", "red");
+});
+
+function TriggerToast(message,error,colour){
+  console.log("triggered");
+  toastTitle.innerHTML = error
+  toastBody.innerHTML = message
+  toastMessage.style.backgroundColor = colour
+  toastImg.src = "../fish/spinning-fish.gif"
+  
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastMessage)
+  toastBootstrap.show()
+}
